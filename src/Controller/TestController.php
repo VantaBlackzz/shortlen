69f5ffa8d0481cd9f1fef56ee829478f4c3e64b0
Vbzz <vantablackzz@yandex.ru>
2024-03-09 20:59:06 +0400

    initial

---
+++ src/Controller/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TestController extends AbstractController
+{
+    #[Route(path: '/', methods: ['GET'])]
+    public function action(): Response
+    {
+        return $this->json(['test' => 'yeap']);
+    }
+}
